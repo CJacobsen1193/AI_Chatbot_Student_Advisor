c1c5e26eb67563a98995323f0806b20410eb1de5
CJacobsen1193 <123473032+CJacobsen1193@users.noreply.github.com>
2024-04-30 13:41:17 -0400

    Update streamlit.py
    Added load_db()

--- streamlit.py
+++ streamlit.py
@@ -12,7 +12,6 @@ def load_model():
     load_in_4bit=True,
     bnb_4bit_compute_dtype=torch.float16,
   )
-
   llm = HuggingFaceLLM(
     model_name="meta-llama/Llama-2-7b-chat-hf",
     tokenizer_name="meta-llama/Llama-2-7b-chat-hf",
@@ -23,8 +22,29 @@ def load_model():
     device_map="auto",
   )
 
+def load_db():
+  from llama_index.core import VectorStoreIndex
+  from llama_index.vector_stores.qdrant import QdrantVectorStore
+  import qdrant_client
+  from qdrant_client import QdrantClient
+  from llama_index.core import Settings
+  from InstructorEmbedding import INSTRUCTOR
+
+  Settings.llm = llm
+  Settings.embed_model='local:hkunlp/instructor-large'
+
+
+  qdrant_client = QdrantClient(
+    url="https://1d752ae2-4e0f-4101-ae0f-b59cd212e480.us-east4-0.gcp.cloud.qdrant.io",
+    api_key="ZEUHVnqv9sKXF1gHpY3u1pBKljE26BBoOqA2bkyAXKT7nEhCdq_xWA",
+  )
+
+  vector_store = QdrantVectorStore(client=qdrant_client, collection_name="mycollection", enable_hybrid=True)
+  index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
+
 def main():
   load_model()
+  load_db()
   
 
 
