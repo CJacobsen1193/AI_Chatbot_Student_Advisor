436f4e7c4b0a139b96558a7137882273cba041f4
CJacobsen1193 <123473032+CJacobsen1193@users.noreply.github.com>
2024-04-30 14:37:44 -0400

    Update streamlit.py
    Try qdrant client

--- streamlit.py
+++ streamlit.py
@@ -45,6 +45,35 @@ def load_db():
 def main():
   load_model()
   load_db()
+
+  st.title("Franklin Virtual Assistant")
+
+  if "huggingface_model" not in st.session_state:
+    st.session_state["huggingface_model"] = llm
+
+  if "messages" not in st.session_state:
+    st.session_state.messges = []
+
+  for message in st.session_state.messages:
+    with st.chat_message(message["role"]:
+      st.markdown(message["content"])
+
+  if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+      st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+      stream = qdrant_client.chat.completions.create(
+        model=st.session_state["huggingface_model"],
+        messages=[
+          {"role": m["role"], "content": m["content"]}
+          for m in st.session_state.messages
+        ],
+        stream=True,
+      )
+      response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
   
 
 
