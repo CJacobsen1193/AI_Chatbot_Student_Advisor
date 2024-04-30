247b48ca69f26593a9a47981981a61a8ed6d8e3c
CJacobsen1193 <123473032+CJacobsen1193@users.noreply.github.com>
2024-04-30 15:41:42 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -53,7 +53,7 @@ def stMain():
     st.session_state["huggingface_model"] = "meta-llama/Llama-2-7b-chat-hf"
 
   if "messages" not in st.session_state:
-    st.session_state.messges = []
+    st.session_state.messages = []
 
   for message in st.session_state.messages:
     with st.chat_message(message["role"]):
