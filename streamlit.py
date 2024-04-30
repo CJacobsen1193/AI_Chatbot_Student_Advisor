4f5b98501ca8e3a0c1f2e9e0bab29a68ad9ebbcc
CJacobsen1193 <123473032+CJacobsen1193@users.noreply.github.com>
2024-04-29 21:21:01 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -13,8 +13,6 @@ def load_model():
   quantization_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_compute_dtype=torch.float16,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_use_double_quant=True,
   )
 
   llm = HuggingFaceLLM(
