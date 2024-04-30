a0b4ad311381966e1749e43285ce2c63f844fdba
CJacobsen1193 <123473032+CJacobsen1193@users.noreply.github.com>
2024-04-30 16:26:17 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,16 +7,12 @@ from llama_index.llms.huggingface import HuggingFaceLLM
 
 
 def load_model():
-  quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.float16,
-  )
+ 
   llm = HuggingFaceLLM(
     model_name="meta-llama/Llama-2-7b-chat-hf",
     tokenizer_name="meta-llama/Llama-2-7b-chat-hf",
     query_wrapper_prompt=PromptTemplate("<s> [INST] {query_str} [/INST] "),
-    context_window=3900,
-    model_kwargs={"token": "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", "quantization_config": quantization_config},
+    context_window=2000,
     tokenizer_kwargs={"token": "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"},
     device_map="auto",
   )
@@ -29,7 +25,6 @@ def load_db():
   from llama_index.core import Settings
   from InstructorEmbedding import INSTRUCTOR
 
-  llm = HuggingFaceLLM("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")
   Settings.llm = llm
   Settings.embed_model='local:hkunlp/instructor-large'
 
@@ -44,7 +39,7 @@ def load_db():
 
 def stMain():
 
-  #load_model()
+  load_model()
   load_db()
   chat_engine = index.as_chat_engine(chat_mode="context",response_mode="compact",max_new_tokens=1024,
                                         system_prompt=("You are a chatbot, able to have normal interactions, as well as talk about Franklin University")
