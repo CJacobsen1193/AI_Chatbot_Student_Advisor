9e2fa37e203e06437d545fc64e1547a4ae7b78eb
CJacobsen1193 <123473032+CJacobsen1193@users.noreply.github.com>
2024-04-29 20:18:22 -0400

    Update streamlit.py
    import accelerate

--- streamlit.py
+++ streamlit.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import torch
 import os
 from transformers import BitsAndBytesConfig
+from transformers import accelerate
 from llama_index.core.prompts import PromptTemplate
 from llama_index.llms.huggingface import HuggingFaceLLM
 
