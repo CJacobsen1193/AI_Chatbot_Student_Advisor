6d3ab8c03658bc8ead085492db86a68d0cd84765
CJacobsen1193 <123473032+CJacobsen1193@users.noreply.github.com>
2024-05-01 14:39:03 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -13,53 +13,69 @@ from InstructorEmbedding import INSTRUCTOR
 
 
 def stMain():
-   llm = HuggingFaceLLM(
-    model_name="meta-llama/Llama-2-7b-chat-hf",
-    tokenizer_name="meta-llama/Llama-2-7b-chat-hf",
-    query_wrapper_prompt=PromptTemplate("<s> [INST] {query_str} [/INST] "),
-    context_window=2000,
-    model_kwargs={"token":"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"},
-    tokenizer_kwargs={"token": "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"},
-    device_map="auto",
+
+  open_api_key = "sk-proj-JbT2fOaShXli4PGSCAmFT3BlbkFJ6LWbRamHlQT8Zx8iR6Rx" 
+  os.environ['OPENAI_API_KEY'] = open_api_key 
+
+  qdrant_client = QdrantClient(
+      url="https://097d6aff-312a-41fe-90e7-90219bf4a194.us-east4-0.gcp.cloud.qdrant.io",
+      api_key="zeQHBdKQ5eZcgopVPI7uNVmisVDMJ4waGlfHjeAEU801klh-b35cIw",
   )
 
-   Settings.llm = llm
-   Settings.embed_model='local:hkunlp/instructor-large'
- 
-   qdrant_client = QdrantClient(
-      url="https://1d752ae2-4e0f-4101-ae0f-b59cd212e480.us-east4-0.gcp.cloud.qdrant.io",
-      api_key="ZEUHVnqv9sKXF1gHpY3u1pBKljE26BBoOqA2bkyAXKT7nEhCdq_xWA",
-   )
+  vector_store = QdrantVectorStore(client=qdrant_client, collection_name="mycollection", enable_hybrid=True)
+  index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
 
-   vector_store = QdrantVectorStore(client=qdrant_client, collection_name="mycollection", enable_hybrid=True)
-   index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
-
-   chat_engine = index.as_chat_engine(chat_mode="context",response_mode="compact",max_new_tokens=1024,
+  chat_engine = index.as_chat_engine(chat_mode="context",response_mode="compact",max_new_tokens=1024,
                                         system_prompt=("You are a chatbot, able to have normal interactions, as well as talk about Franklin University")
                                         )
   
-   st.title("Franklin Virtual Assistant")
+  st.title("Franklin Virtual Assistant")
 
+  #initialize chat history
+  if "messages" not in st.session_state:
+    st.session_state.messages=[]
 
-   if "huggingface_model" not in st.session_state:
-    st.session_state["huggingface_model"] = "meta-llama/Llama-2-7b-chat-hf"
-
-   if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-   for message in st.session_state.messages:
+  for message in st.session_state.messages:
     with st.chat_message(message["role"]):
       st.markdown(message["content"])
 
-   if prompt := st.chat_input("Ask me a question about Franklin University!"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
+  #request prompt from user
+  prompt = None
+  st_message = st.chat_message("user")
+  prompt = st_message.chat_input("Ask me a question about Franklin University")
+  more = True
+
+  #wait for prompt from user
+  with st.sidebar.status("Thinking..."):
+    while more == True:
+      if (prompt is None or len(prompt) <= 0):
+        more = True
+      else:
+        more = False
+  
+  #write prompt on chat window
+  if prompt is not None or len(prompt) > 0:
+    prompt = str(prompt)
+    st.session_state.messages.append(
+      {"role": "user", "content": prompt}
+    )
     with st.chat_message("user"):
       st.markdown(prompt)
 
-    with st.chat_message("assistant"):
-      response = chat_engine.chat(prompt)
-      st.session_state.messages.append({"role": "assistant", "content": response})
-      
+  #display response in chat message container
+  response = None
+  with st.sidebar.status("Finding answers..."):
+    response = chat_engine.chat(prompt)
+  if response is not None or len(response) > 0:
+    response = str(response)
+    if prompt and response:
+      st_message = st.chat_message("assistant")
+      st_message.write(response.response)
+      st.session_state.messages.append(
+        {"role" : "assistant",
+        "content": response.response}
+      )
+
 
 if __name__ == "__main__":
   stMain()
