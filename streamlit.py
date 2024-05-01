80123635a891a18bcf8743fb18b7f2893b7be786
CJacobsen1193 <123473032+CJacobsen1193@users.noreply.github.com>
2024-04-30 20:50:42 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -17,41 +17,41 @@ def stMain():
     device_map="auto",
   )
 
-  from llama_index.core import VectorStoreIndex
-  from llama_index.vector_stores.qdrant import QdrantVectorStore
-  import qdrant_client
-  from qdrant_client import QdrantClient
-  from llama_index.core import Settings
-  from InstructorEmbedding import INSTRUCTOR
+   from llama_index.core import VectorStoreIndex
+   from llama_index.vector_stores.qdrant import QdrantVectorStore
+   import qdrant_client
+   from qdrant_client import QdrantClient
+   from llama_index.core import Settings
+   from InstructorEmbedding import INSTRUCTOR
 
-  Settings.llm = llm
-  Settings.embed_model='local:hkunlp/instructor-large'
+   Settings.llm = llm
+   Settings.embed_model='local:hkunlp/instructor-large'
  
-  qdrant_client = QdrantClient(
-    url="https://1d752ae2-4e0f-4101-ae0f-b59cd212e480.us-east4-0.gcp.cloud.qdrant.io",
-    api_key="ZEUHVnqv9sKXF1gHpY3u1pBKljE26BBoOqA2bkyAXKT7nEhCdq_xWA",
-  )
+   qdrant_client = QdrantClient(
+      url="https://1d752ae2-4e0f-4101-ae0f-b59cd212e480.us-east4-0.gcp.cloud.qdrant.io",
+      api_key="ZEUHVnqv9sKXF1gHpY3u1pBKljE26BBoOqA2bkyAXKT7nEhCdq_xWA",
+   )
 
-  vector_store = QdrantVectorStore(client=qdrant_client, collection_name="mycollection", enable_hybrid=True)
-  index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
+   vector_store = QdrantVectorStore(client=qdrant_client, collection_name="mycollection", enable_hybrid=True)
+   index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
 
-  chat_engine = index.as_chat_engine(chat_mode="context",response_mode="compact",max_new_tokens=1024,
+   chat_engine = index.as_chat_engine(chat_mode="context",response_mode="compact",max_new_tokens=1024,
                                         system_prompt=("You are a chatbot, able to have normal interactions, as well as talk about Franklin University")
                                         )
   
-  st.title("Franklin Virtual Assistant")
+   st.title("Franklin Virtual Assistant")
 
-  if "huggingface_model" not in st.session_state:
+   if "huggingface_model" not in st.session_state:
     st.session_state["huggingface_model"] = "meta-llama/Llama-2-7b-chat-hf"
 
-  if "messages" not in st.session_state:
+   if "messages" not in st.session_state:
     st.session_state.messages = []
 
-  for message in st.session_state.messages:
+   for message in st.session_state.messages:
     with st.chat_message(message["role"]):
       st.markdown(message["content"])
 
-  if prompt := st.chat_input("Ask me a question about Franklin University!"):
+   if prompt := st.chat_input("Ask me a question about Franklin University!"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
       st.markdown(prompt)
